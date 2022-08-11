05c262218af23f1d1e5b4645cc8059ac397f3b28
iii706 <295080653@qq.com>
2022-08-11 23:47:29 +0800

    修复get_seller_info，每次获取数据

--- spider/get_seller_info.py
+++ spider/get_seller_info.py
@@ -44,7 +44,7 @@ def get_page_content(seller_id):
             continue
 
 while True:
-    sellerbases = SellerBase.objects.filter(last_product_counts=0).order_by("-id").exclude(country='CN')[:1]
+    sellerbases = SellerBase.objects.filter(last_product_counts=0).order_by("mod_time").exclude(country='CN')[:1]
     if len(sellerbases) > 0:
         sellerbase = sellerbases[0]
         crawl_day = (timezone.now() - sellerbase.mod_time).days
@@ -53,26 +53,23 @@ while True:
         print(seller_id)
         if seller_id == '':
             continue
-        if add_mod_day == 0 or crawl_day >= 7:
-            res = get_page_content(seller_id)
-            selector = etree.HTML(res.text)
-            title = selector.xpath("//title/text()")
-            sellerbase.brand_name = ''.join(selector.xpath('//*[@id="sellerName-rd"]/text()'))
-            ratings_infos = selector.xpath('//table[@id="feedback-summary-table"]//tr[5]//td//text()')
-            if len(ratings_infos) == 5:
-                sellerbase.last_days_30_ratings = ratings_infos[1].replace(",", "")
-                sellerbase.last_days_90_ratings = ratings_infos[2].replace(",", "")
-                sellerbase.last_year_ratings = ratings_infos[3].replace(",", "")
-                sellerbase.last_life_ratings = ratings_infos[4].replace(",", "")
-            sellerbase.business_name = ''.join(
-                selector.xpath('//div[@id="page-section-detail-seller-info"]/div/div/div/div[2]/span[2]/text()'))
-            sellerbase.business_addr = "|".join(selector.xpath('//div[@class="a-row a-spacing-none indent-left"]//text()'))
-            sellerbase.country = sellerbase.business_addr.split('|')[-1]
-            # seller.add_time =
-            print(sellerbase.brand_name, ratings_infos, sellerbase.business_name, sellerbase.business_addr)
-            sellerbase.save()
-        else:
-            print("上次抓取时间没有超过7天，不用更新", seller_id, sellerbase.mod_time)
+        res = get_page_content(seller_id)
+        selector = etree.HTML(res.text)
+        title = selector.xpath("//title/text()")
+        sellerbase.brand_name = ''.join(selector.xpath('//*[@id="sellerName-rd"]/text()'))
+        ratings_infos = selector.xpath('//table[@id="feedback-summary-table"]//tr[5]//td//text()')
+        if len(ratings_infos) == 5:
+            sellerbase.last_days_30_ratings = ratings_infos[1].replace(",", "")
+            sellerbase.last_days_90_ratings = ratings_infos[2].replace(",", "")
+            sellerbase.last_year_ratings = ratings_infos[3].replace(",", "")
+            sellerbase.last_life_ratings = ratings_infos[4].replace(",", "")
+        sellerbase.business_name = ''.join(
+            selector.xpath('//div[@id="page-section-detail-seller-info"]/div/div/div/div[2]/span[2]/text()'))
+        sellerbase.business_addr = "|".join(selector.xpath('//div[@class="a-row a-spacing-none indent-left"]//text()'))
+        sellerbase.country = sellerbase.business_addr.split('|')[-1]
+        # seller.add_time =
+        print(sellerbase.brand_name, ratings_infos, sellerbase.business_name, sellerbase.business_addr)
+        sellerbase.save()
     else:
         print('没有需要更新的卖家数据')
         time.sleep(60*3)
